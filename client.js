de68a26d287f3960fca646a4985adf4230c3eb89
Bea Mungcal <beamungcal@hotmail.com>
2023-05-10 18:13:46 -0400

    Sent Move Up string as data to the server via the conn object.

--- client.js
+++ client.js
@@ -3,22 +3,30 @@ const net = require("net");
 const stdin = process.stdin;
 // establishes a connection with the game server
 const connect = function () {
+  console.log("Connecting ...");
   const conn = net.createConnection({
     host: "localhost",
     port: 50541
   });
-  conn.on("connect", () => {
-    conn.write("Name: BM");
-  });
-  
+
   conn.setEncoding("utf8"); // interpret data as text
   conn.on("data", (data) => {
     console.log("Message from client: ", data);
   });
+
+  conn.on("connect", () => {
+    console.log('I have connected!!!');
+    conn.write("Name: BM");
+  });
+
+  // conn.on("connect", () => {
+  //   setTimeout(() => {
+  //     conn.write("Move: up",);
+  //   }, 2000);
+  // });
     return conn;
 };
 
-console.log("Connecting ...");
 // connect();
 
 module.exports = {connect};
