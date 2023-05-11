419133fd9c64839930fa47f777564586c6028566
Bea Mungcal <beamungcal@hotmail.com>
2023-05-11 13:02:06 -0400

    Assigned functions to keys(data).

--- input.js
+++ input.js
@@ -1,10 +1,23 @@
+let connection;
+
 const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
+  } else if (data === 'w') {
+    connection.write("Move: up");
+  } else if (data === 'a') {
+    connection.write("Move: down");
+  } else if (data === 's') {
+    connection.write("Move: left");
+  } else if (data === 'd') {
+    connection.write("Move: right");
+  } else if (data === 'f') {
+    connection.write(`Say: Hola`);
   }
 };
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,4 +26,4 @@ const setupInput = function () {
   return stdin;
 };
 
-module.exports = {setupInput};
+module.exports = {setupInput, handleUserInput};
