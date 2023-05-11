91d80df359d2c70f7f8ff009668eb5849e033eee
Bea Mungcal <beamungcal@hotmail.com>
2023-05-10 20:46:38 -0400

    Added a function to check for the termination key in order to exit the game.

--- play.js
+++ play.js
@@ -1,6 +1,22 @@
 const {connect} = require('./client');
-const connection = connect();
+//const connection = connect();
 //console.log(connect);
 // connection.on('connect', function() {
-//   console.log('I have connected!!!');
 // });
+connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput)
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+setupInput();
