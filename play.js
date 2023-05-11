0d3c9e011583691ff11208e55e5558f5491aac7f
Bea Mungcal <beamungcal@hotmail.com>
2023-05-10 21:17:27 -0400

    Refactored all included functions.

--- play.js
+++ play.js
@@ -1,22 +1,23 @@
-const {connect} = require('./client');
-//const connection = connect();
-//console.log(connect);
-// connection.on('connect', function() {
-// });
+const { connect } = require("./client");
+const { setupInput } = require("./input");
+
+console.log("Connecting ...");
 connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput)
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
 setupInput();
+
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   stdin.on("data", handleUserInput)
+//   return stdin;
+// };
+
+// const handleUserInput = function (data) {
+//   if (data === '\u0003') {
+//     process.exit();
+//   }
+// };
+// setupInput();
